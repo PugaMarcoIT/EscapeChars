bfda5e7e993b4349b6c57f4f44dabe84604acc39
PugaMarcoIT <l20140182@queretaro.tecnm.mx>
2022-09-10 16:36:37 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace EscapeChars // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string result ="";
+            Console.WriteLine("Nueva linea");
+            result = "Marco\nPuga\nBlanco";
+            Console.WriteLine(result);
+        }
+    }
+}
